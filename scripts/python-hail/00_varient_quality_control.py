387b5ae853aafbb69ffadd0fd2b220f0e9ff21e1
shyamrav <88066344+shyamrav@users.noreply.github.com>
2022-09-02 17:37:49 +1000

    Adding variant_qc step and allow hail bucket access

--- scripts/python-hail/00_varient_quality_control.py
+++ scripts/python-hail/00_varient_quality_control.py
@@ -1,5 +1,9 @@
 import hail as hl
-hl.init(default_reference='GRCh38')
+hl.init(default_reference='GRCh38',
+       spark_conf={
+           'spark.hadoop.fs.gs.requester.pays.mode': 'CUSTOM',
+           'spark.hadoop.fs.gs.requester.pays.buckets': 'hail-datasets-us',
+           'spark.hadoop.fs.gs.requester.pays.project.id': '1050631177138'})
 
 mt = hl.read_matrix_table('gs://activestorage_amppd_mgrb/v1-2-amp-pd-mgrb.sparse.mt/')
 
@@ -23,9 +27,10 @@ mt = mt.filter_rows(mt.info.QUALapprox > 90)
 mt = mt.annotate_rows(hwe = hl.agg.hardy_weinberg_test(mt.GT))
 mt = mt.filter_rows(mt.hwe.p_value > 0.001)
 
+# Run variant QC 
+mt = hl.variant_qc(mt)
 # Filter by call_rate lower than 0.9
 mt = mt.filter_rows(mt.variant_qc.call_rate > 0.9)
-
 
 # Annotate matrix with dbNSFP annotations
 db = hl.experimental.DB(region='us', cloud='gcp')
