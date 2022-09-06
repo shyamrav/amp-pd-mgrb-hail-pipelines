5f4be257ed7a4a0e9a1a80af65b102f935682ce8
shyamrav <88066344+shyamrav@users.noreply.github.com>
2022-09-06 11:28:53 +1000

    Equals to as well

--- scripts/python-hail/00_varient_quality_control.py
+++ scripts/python-hail/00_varient_quality_control.py
@@ -15,22 +15,22 @@ mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
 
 # Filter out rows with less than 10 DP
 # Rows with missing values are removed regardless of keep
-mt = mt.filter_rows(mt.info.DP > 10, keep=True)
+mt = mt.filter_rows(mt.info.DP >= 10, keep=True)
 
 # Filter by PASS status
 mt = mt.filter_rows(mt.filters == {"PASS"})
 
 # Filter by Q values greater than 90
-mt = mt.filter_rows(mt.info.QUALapprox > 90)
+mt = mt.filter_rows(mt.info.QUALapprox >= 90)
 
 # Hardy-Weinberg equilibrium greater than 0.001
 mt = mt.annotate_rows(hwe = hl.agg.hardy_weinberg_test(mt.GT))
-mt = mt.filter_rows(mt.hwe.p_value > 0.001)
+mt = mt.filter_rows(mt.hwe.p_value >= 0.001)
 
 # Run variant QC 
 mt = hl.variant_qc(mt)
 # Filter by call_rate lower than 0.9
-mt = mt.filter_rows(mt.variant_qc.call_rate > 0.9)
+mt = mt.filter_rows(mt.variant_qc.call_rate >= 0.9)
 
 # Annotate matrix with dbNSFP annotations
 db = hl.experimental.DB(region='us', cloud='gcp')
